3e354c6599a149c52fff98a8fc654bad733dfeee
sein0709 <psein0709@gmail.com>
2023-10-07 00:34:28 +0900

    UI update
    Added UI

---
+++ Untitle_miniProject/Assets/Scripts/Setting/VolumeSetting.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSetting : MonoBehaviour
+{
+    [SerializeField] public Slider musicSlider;
+    [SerializeField] public Slider SFXSlider;
+    public AudioMixer mixer;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        mixer.SetFloat("music", volume);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        mixer.SetFloat("SFX", volume);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+}
