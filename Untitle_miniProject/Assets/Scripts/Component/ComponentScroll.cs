77771cd26b2555152d4630d26749c05518f14430
rnqhscjf3333 <41632370+rnqhscjf3333@users.noreply.github.com>
2023-10-08 11:43:20 +0900

    스크롤 수정

--- Untitle_miniProject/Assets/Scripts/Component/ComponentScroll.cs
+++ Untitle_miniProject/Assets/Scripts/Component/ComponentScroll.cs
@@ -16,12 +16,23 @@ public class ComponentScroll : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     public void OnDrag(PointerEventData eventData)
     {
-        
+        if (scrollbar.value < -0.5f)//너무 위나 아래로 드래그했으면 드래그 끝내기
+        {
+            eventData.pointerDrag = null;
+            targetPos = 0f;
+            isdrag = false;
+        }
+        else if (scrollbar.value > 1.2f)
+        {
+            eventData.pointerDrag = null;
+            targetPos = 1f;
+            isdrag = false;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (scrollbar.value < 0.5f)
+        if (scrollbar.value < 0.5f)//드래그 끝났을때 절반이상 넘어갔으면 해당위치로 타겟
         {
             targetPos = 0f;
         }
@@ -34,6 +45,6 @@ public class ComponentScroll : MonoBehaviour, IBeginDragHandler, IDragHandler, I
     {
         if (!isdrag)
             //scrollbar.value = targetPos;
-            scrollbar.value = Mathf.Lerp(scrollbar.value, targetPos, 0.1f);
+            scrollbar.value = Mathf.Lerp(scrollbar.value, targetPos, 0.1f);//타겟위치로 부드럽게 이동
     }
 }
