b21f168580b8971e2b791b0f1db21d61a380d523
damdadi93 <77727581+damdadi93@users.noreply.github.com>
2023-10-10 02:49:26 +0900

    make Stage selet
    맵 선택기능 만듬 일단 기본적인 기능 추후에 피드백

---
+++ Untitle_miniProject/Assets/Scripts/UI_Button/StageSelector.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageSelector : MonoBehaviour
+{
+    public static int selectedStage;
+    public int stage;
+    //public string word;
+    public TextMeshProUGUI stageText;
+    
+
+   
+    void Awake()
+    {
+        stageText = GetComponent<TextMeshProUGUI>();
+
+        //stageText.text = stage.ToString();
+        //if(stage == 0)
+        //{
+        //    word = "T";
+        //    stageText.text = word;
+
+        //}
+        if(stageText != null )
+        {
+            stageText.text = stage.ToString();
+        }
+        else
+        {
+            //Debug.LogError("StageText not found!");
+        }
+    }
+
+    public void OpenScene()    
+    {
+        selectedStage = stage;
+        
+        if (stage == 0)
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        else
+        {
+            SceneManager.LoadScene("Stage" + stage.ToString());
+        }
+    }
+
+    public void BackScene() 
+    {
+        SceneManager.LoadScene("SceneSelect");
+    }
+
+}
---
+++ Untitle_miniProject/Assets/Scripts/UI_Button/UniversalStageManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UniversalStageManager : MonoBehaviour
+{
+
+    //게임 배경화면 씬별로 불러오기
+    public Sprite[] backgrounds;
+    public Image background;
+    public TextMeshProUGUI textMeshProUGUI;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int stage = StageSelector.selectedStage;
+        Debug.Log(stage);
+        textMeshProUGUI.text = "Stage" + stage.ToString();
+        background.sprite = backgrounds[stage - 1];
+    }
+
+   
+    public void GoBackToStageSelection()
+    {
+        SceneManager.LoadScene("SceneSelect");
+    }
+}
